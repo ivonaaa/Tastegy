3cbf3eaee96af13008a8c7c13f99b30d885e36f8
ivona.perko <58182020+ivonaaa@users.noreply.github.com>
2024-06-01 13:14:29 +0200

    Style buttons in form

--- frontend/src/components/RecipeDetails.js
+++ frontend/src/components/RecipeDetails.js
@@ -1,8 +1,11 @@
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
 const RecipeDetails = ({ recipe }) => {
     return (
         <div className="recipe-details">
             <h4>{recipe.title}</h4>
             <p><strong>Details: </strong>{recipe.description}</p>
+            <p>{ formatDistanceToNow(new Date(recipe.createdAt), {addSuffix: TextTrackCue}) }</p>
         </div>
     )
 }

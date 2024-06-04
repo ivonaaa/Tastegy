104130c2d2a27daeacbd4654525a709aadda0e5b
ivona.perko <58182020+ivonaaa@users.noreply.github.com>
2024-06-04 19:47:23 +0200

    Modify recipe form

--- frontend/src/components/RecipeForm.js
+++ frontend/src/components/RecipeForm.js
@@ -50,64 +50,70 @@ const RecipeForm = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="title">Title:</label>
-                <input
-                    type="text"
-                    id="title"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                    required
-                />
-            </div>
-            <div>
-                <label htmlFor="description">Description:</label>
-                <textarea
-                    id="description"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                    required
-                    rows="4"
-                />
-            </div>
-            <div>
-                <label>Ingredients:</label>
-                {ingredients.map((ingredient, index) => (
-                    <div key={index}>
-                        <input
-                            type="text"
-                            name="name"
-                            placeholder="Name"
-                            value={ingredient.name}
-                            onChange={(e) => handleIngredientChange(index, e)}
-                            required
-                        />
-                        <input
-                            type="number"
-                            name="quantity"
-                            placeholder="Quantity"
-                            value={ingredient.quantity}
-                            onChange={(e) => handleIngredientChange(index, e)}
-                            required
-                        />
-                        <input
-                            type="text"
-                            name="unit"
-                            placeholder="Unit"
-                            value={ingredient.unit}
-                            onChange={(e) => handleIngredientChange(index, e)}
-                            required
-                        />
+        <div className='recipeForm'>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="title">Title:</label>
+                    <input
+                        type="text"
+                        id="title"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="description">Step by step guide:</label>
+                    <textarea
+                        id="description"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        required
+                        rows="4"
+                    />
+                </div>
+                <div>
+                    <label>
+                        Ingredients: 
+                        <span onClick={handleAddIngredient} className='material-symbols-outlined addbutton'>
+                            add_circle
+                        </span>
+                    </label>
+                    <div className='ingredients'>
+                        {ingredients.map((ingredient, index) => (
+                            <div key={index}>
+                                <input
+                                    type="text"
+                                    name="name"
+                                    placeholder="Name"
+                                    value={ingredient.name}
+                                    onChange={(e) => handleIngredientChange(index, e)}
+                                    required
+                                />
+                                <input
+                                    type="number"
+                                    name="quantity"
+                                    placeholder="Quantity"
+                                    value={ingredient.quantity}
+                                    onChange={(e) => handleIngredientChange(index, e)}
+                                    required
+                                />
+                                <input
+                                    type="text"
+                                    name="unit"
+                                    placeholder="Unit"
+                                    value={ingredient.unit}
+                                    onChange={(e) => handleIngredientChange(index, e)}
+                                    required
+                                />
+                            </div>
+                        ))}
                     </div>
-                ))}
-                <button type="button" onClick={handleAddIngredient}>
-                    Add Ingredient
-                </button>
-            </div>
-            <button type="submit">Submit Recipe</button>
-            {error && <div className='error'>{error}</div>}
-        </form>
+                </div>
+                <button type="submit">Submit Recipe</button>
+                {error && <div className='error'>{error}</div>}
+            </form>
+        </div>
     );
 };
 
